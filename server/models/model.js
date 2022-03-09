cbe4019f854494ae9455f4c9fd13375fae77d5b1
Varun <korannevarun@gmail.com>
2022-03-10 02:18:38 +0530

    Created basic login and signup requests

--- server/models/model.js
+++ server/models/model.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create schema for todo
-const TodoSchema = new Schema({
-  action: {
+const UserSchema = new mongoose.Schema({
+  name: {
     type: String,
-    required: [true, 'The todo text field is required'],
-  },
-});
-
-// Create model for todo
-const Todo = mongoose.model('todo', TodoSchema);
-
-module.exports = Todo;
+    index: {unique: true}
+  }, 
+   email: {
+      type: String,
+      index: { unique: true }
+    },
+    password: String
+    
+  });
+  
+  module.exports = mongoose.model('User', UserSchema);
+  
+  
